785c69f91a43c3eeacf9afba4e664c84ef2de1ff
Bestin <bestinjohn1993@gmail.com>
2017-09-03 12:59:41 +0530

    Specs for Async Tasks

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,19 +1,37 @@
 describe("Address Book", function () {
     var addressBook,
         thisContact;
-    beforeEach(function(){
+    beforeEach(function () {
         addressBook = new AddressBook();
         thisContact = new Contact();
-    });   
-    it("should be able to add a contact", function () { 
+    });
+    it("should be able to add a contact", function () {
         addressBook.addContact(thisContact);
 
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
-    it("should be able to delete a contact", function () { 
+    it("should be able to delete a contact", function () {
         addressBook.addContact(thisContact);
         addressBook.deleteContact(0);
 
         expect(addressBook.getContact(0)).not.toBeDefined();
+    });
+});
+
+describe("Async Address Book", function () {
+    var addressBook = new AddressBook();
+    
+    //Done is used to indicate that its an async task
+    beforeEach(function(done){
+        addressBook.getInitialContacts(function(){
+            //Tells our tests async task is completed
+            done();            
+        });
+    });
+
+    //The done param here indicates that this test should be run once done() is executed.
+    it("should grab initial contacts", function (done) {     
+        expect(addressBook.initialComplete).toBe(true);
+        done();
     });
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,14 +1,23 @@
 function AddressBook() {
     var self = this;
     self.contacts = [];
+    self.initialComplete = false;
+    self.getInitialContacts = function (cb) {
+        setTimeout(function () {
+            self.initialComplete = true;
+            if (cb) {
+                return cb();
+            }
+        }, 3);
+    };
     self.addContact = function (contact) {
         self.contacts.push(contact);
     };
 
-    self.getContact = function(index){
+    self.getContact = function (index) {
         return self.contacts[index];
     };
-    self.deleteContact = function(index){
-        self.contacts.splice(index,1);
-    };  
+    self.deleteContact = function (index) {
+        self.contacts.splice(index, 1);
+    };
 }
